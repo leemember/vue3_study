04214d2df1116861865e20efae1253dcc81a1f69
leehyunju <hyunmail521@gmail.com>
2024-02-12 19:46:58 +0900

    vue axios

--- 초급/vue3-form/README.md
+++ 초급/vue3-form/README.md
@@ -1,24 +1,17 @@
 # vue3-form
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+- Fake JSON 사용
 
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+> https://jsonplaceholder.typicode.com/user/
--- 초급/vue3-form/src/App.vue
+++ 초급/vue3-form/src/App.vue
@@ -2,19 +2,21 @@
   <div>
     <form action="" @submit="submitForm">
       <div>
-        <label for="id">ID : </label>
+        <label for="userId">ID : </label>
         <input type="text" id="userId" v-model="username" />
       </div>
       <div>
         <label for="password">PW : </label>
         <input type="text" id="password" v-model="password" />
       </div>
-      <button type="submit">로그인</button>
+      <button type="submit" @click="logText">로그인</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
@@ -23,10 +25,22 @@ export default {
     };
   },
   methods: {
-    submitForm() {
+    // event.preventDefault(); 이걸 생략하는 방법중에서는
+    //  <form action="" @submit="submitForm"> 이 부분에 @submit.prevent라고 하면 된다.
+    submitForm(event) {
+      event.preventDefault();
       console.log("제출됨");
-      this.username;
-      this.password;
+
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/users/", {
+          data,
+        })
+        .then((res) => console.log(res));
     },
   },
 };
