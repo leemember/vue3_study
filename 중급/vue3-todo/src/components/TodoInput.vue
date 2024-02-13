0c8f8e17aba50b68ed683170cf713b9350208aba
leehyunju <hyunmail521@gmail.com>
2024-02-14 00:56:33 +0900

    todoinput setting

--- 중급/vue3-todo/src/components/TodoInput.vue
+++ 중급/vue3-todo/src/components/TodoInput.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <input type="text" v-model="todoInput" />
+    <button @click="addTodo">추가</button>
+  </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const todoInput = ref("");
+
+    // methods
+    function addTodo() {
+      const todo = todoInput.value;
+      localStorage.setItem(todo, todo);
+    }
+
+    // setup은 무조건 return 해줘야함 !!! 주의!!!
+    return { todoInput, addTodo };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
