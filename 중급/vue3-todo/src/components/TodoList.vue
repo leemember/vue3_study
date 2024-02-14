4f299b2ca4da105e290cf47763cadafe9b1aa393
leehyunju <hyunmail521@gmail.com>
2024-02-14 23:10:41 +0900

    feat : todolist componenst

--- 중급/vue3-todo/src/components/TodoList.vue
+++ 중급/vue3-todo/src/components/TodoList.vue
@@ -1,9 +1,20 @@
 <template>
-  <div></div>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+  </ul>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  setup() {
+    // 데이터 초기값
+    const items = ref(['애플','삼성','인프런']);
+
+    return { items };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
