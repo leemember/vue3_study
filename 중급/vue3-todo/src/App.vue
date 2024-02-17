2fd924d35e5601c38f97f142bde184ca827218e3
leehyunju <hyunmail521@gmail.com>
2024-02-17 13:50:02 +0900

    refectore

--- 중급/vue3-todo/src/App.vue
+++ 중급/vue3-todo/src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <TodoHeader></TodoHeader>
   <TodoInput></TodoInput>
-  <TodoList></TodoList>
+  <TodoList :todoItems="todoItems"></TodoList>
 </template>
 
-<script>
+<script setup>
 import TodoHeader from "@/components/TodoHeader.vue";
 import TodoInput from "@/components/TodoInput.vue";
 import TodoList from "@/components/TodoList.vue";
+import { ref } from "vue";
 
-export default { components: { TodoHeader, TodoInput, TodoList } };
+// data
+const todoItems = ref([]);
+
+// methods
+function fetchTodos() {
+  const result = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const todoItem = localStorage.key(i);
+    result.push(todoItem);
+  }
+  return result;
+}
+
+todoItems.value = fetchTodos();
 </script>
 
 <style lang="scss" scoped></style>
--- 중급/vue3-todo/src/components/TodoList.vue
+++ 중급/vue3-todo/src/components/TodoList.vue
@@ -1,29 +1,12 @@
 <template>
   <ul>
-    <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    <li v-for="(item, index) in todoItems" :key="index">{{ item }}</li>
   </ul>
 </template>
 
 <script>
-import { ref } from "vue";
-
 export default {
-  setup() {
-    // 데이터 초기값
-    const items = ref(["애플", "삼성", "인프런"]);
-
-    // methods
-    function fetchTodos() {
-      for (let i = 0; i < localStorage.length; i++) {
-        const todoItem = localStorage.key(i);
-        items.value.push(todoItem);
-      }
-    }
-
-    fetchTodos();
-
-    return { items, fetchTodos };
-  },
+  props: ["todoItems"],
 };
 </script>
 
